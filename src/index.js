31c800180242f67e61a8eca5b9de0cd4a947197a
aadv1k <aadv1k@outlook.com>
2023-08-28 13:51:30 +0530

    Early implementation for selecting stuff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 export class Spreadsheet {
     constructor() {
-        this.width = 900;
         this.height = 400;
 
         this.maxCellX = 100;
@@ -8,6 +7,8 @@ export class Spreadsheet {
 
         this.cellWidth = 60;
         this.cellHeight = 15;
+
+        this.width = 900 + this.cellWidth/2;
     }
 
     renderCell(context, x, y) {
@@ -25,6 +26,19 @@ export class Spreadsheet {
         context.stroke();
     }
 
+    getColumnName(index) {
+        let columnName = "";
+        let quotient = index;
+
+        while (quotient >= 0) {
+            const remainder = quotient % 26;
+            columnName = String.fromCharCode(65 + remainder) + columnName;
+            quotient = Math.floor(quotient / 26) - 1;
+        }
+
+        return columnName;
+    }
+
     getColumnBar() {
         const columnBar = document.createElement("div");
         columnBar.style.display = "flex";
@@ -36,15 +50,14 @@ export class Spreadsheet {
         for (let i = 0; i < this.maxCellX; i++) {
             const columnHead = document.createElement("div");
             columnHead.style.minWidth = this.cellWidth + "px";
-            columnHead.textContent = String.fromCharCode(65 + i); 
+            columnHead.textContent = this.getColumnName(i); 
             columnBar.appendChild(columnHead);
         }
-
         return columnBar;
     }
 
 
-    getRowBar() {
+    getRowBar(onClick) {
         const rowBar = document.createElement("div");
         rowBar.classList.add("ss__row-bar");
 
@@ -53,6 +66,10 @@ export class Spreadsheet {
             rowHead.style.width = this.cellWidth/2 + "px";
             rowHead.style.textAlign = "center";
             rowHead.textContent = `${i + 1}`;
+            rowHead.setAttribute("data-pos", i);
+
+            rowHead.addEventListener("click", onClick);
+
             rowBar.appendChild(rowHead);
         }
 
@@ -73,7 +90,27 @@ export class Spreadsheet {
         container.style.display = "flex";
         container.classList.add("ss__container");
 
-        const rowBar = this.getRowBar();
+        const rowBar = this.getRowBar((e) => {
+            const isRowSelected = Number(e.target.getAttribute("data-selected"));
+
+            if (isRowSelected) {
+                e.target.style.outline = "none";
+
+                const y = e.target.getAttribute("data-pos") * this.cellHeight;
+                const context = canvas.getContext("2d");
+                context.clearRect(0.5, y + 0.5, this.width * this.cellWidth, this.cellHeight); 
+                e.target.setAttribute("data-selected", 0); 
+            } else {
+                e.target.style.outline = "1px solid #191919";
+
+                const y = e.target.getAttribute("data-pos") * this.cellHeight;
+                const context = canvas.getContext("2d");
+                context.strokeStyle = "#191919";
+                context.strokeRect(0.5, y + 0.5, this.width * this.cellWidth, this.cellHeight);
+                e.target.setAttribute("data-selected", 1); 
+            }
+        });
+
         container.appendChild(rowBar);
 
         const canvas = document.createElement("canvas");
